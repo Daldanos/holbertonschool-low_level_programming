fd9092edbd4eaa9542fabeeeb37eadb55845e6f9
Daldanos <adolfogsilveira@gmail.com>
2023-10-10 10:39:25 -0700

    Programming C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - start
+ *
+ * Description: First try to C
+ *
+ * Return: correct output (sucess)
+ */
 int main(void)
 {
 	char Prueba[] = "\"Programming is like building a multilingual puzzle";
