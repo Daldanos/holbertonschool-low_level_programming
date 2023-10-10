a6b0b6a659a729643fa595f1be19905e150afaef
Daldanos <adolfogsilveira@gmail.com>
2023-10-10 10:55:31 -0700

    Programming C

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdioh>
+/**
+ * main - start
+ *
+ * Description:
+ *
+ * Return: Correct output
+ */
+int main(void)
+{
+	printf "with proper grammar, but the outcome is a piece of art,\n";
+
+	return (0);
+}
