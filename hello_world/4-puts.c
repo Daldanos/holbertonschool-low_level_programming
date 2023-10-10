c4addceab5d2620a97d4fa7134e9e748817b208f
Daldanos <adolfogsilveira@gmail.com>
2023-10-10 10:20:02 -0700

    Programming C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+
+/**
+ *
+ * Main function, using puts
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 }
