5d172aa588138be04f68900b23441bbb2c666ab6
Daldanos <adolfogsilveira@gmail.com>
2023-10-11 10:14:43 -0700

    Programming in C

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -15,10 +15,10 @@ int main(void)
 	int PRUEBA;
 
 	for (PRUEBA = 97; PRUEBA <= 122; PRUEBA++)
+	{
+		if (PRUEBA != 101 && PRUEBA != 113)
 		putchar((char)PRUEBA);
-	for (PRUEBA = 65; PRUEBA <= 90; PRUEBA++)
-		putchar((char)PRUEBA);
+	}
 	putchar('\n');
 	return (0);
 }
-
