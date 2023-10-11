7646e2c4972064e6b06fe47100d0f3cef832e95c
Daldanos <adolfogsilveira@gmail.com>
2023-10-11 06:17:05 -0700

    Programming in C

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - start
+*
+* Description:
+*
+* Return: Correct output
+*/
+int main(void)
+{
+	int VARINT;
+	char VARCHAR;
+	float VARFLOAT;
+	double VARDOUBLE;
+
+	printf("Size of int is: %ld", sizeof(VARINT)\n);
+	printf("Size of char is: %ld", sizeof(VARCHAR)\n);
+	printf("Size of float is: %ld", sizeof(VARFLOAT)\n);
+	printf("Size of double is: %ld", sizeof(VARDOUBLE)\n);
+
+	return (0);
+}
