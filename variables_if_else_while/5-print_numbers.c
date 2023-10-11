610b4c30bfb0f6a3d20ae3a765f3cd3a482b0f93
Daldanos <adolfogsilveira@gmail.com>
2023-10-11 10:17:48 -0700

    Programming in C

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - start
+*
+* Description:
+*
+* Return: Correct output
+*/
+
+int main(void)
+{
+	int PRUEBA;
+
+	for (PRUEBA = 0; PRUEBA <= 9; PRUEBA++)
+		putchar((char)PRUEBA);
+	putchar('\n');
+	return (0);
+}
