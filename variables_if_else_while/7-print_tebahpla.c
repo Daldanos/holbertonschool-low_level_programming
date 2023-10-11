7ae6bd2001fbee58bd182a402da2bbfbaed5aad9
Daldanos <adolfogsilveira@gmail.com>
2023-10-11 10:33:28 -0700

    Programming in C

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - start
+*
+* Description:
+*
+* Return: Correct output
+*/
+
+int main(void)
+{
+	int PRUEBA;
+
+	for (PRUEBA = 122; PRUEBA >= 97; PRUEBA--)
+		putchar((char)PRUEBA);
+	putchar('\n');
+	return (0);
+}
