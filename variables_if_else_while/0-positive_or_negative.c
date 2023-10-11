5eb71221597a9778a5efb2d9d67dab7f8782a240
Daldanos <adolfogsilveira@gmail.com>
2023-10-11 07:35:01 -0700

    Programming in C

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -19,13 +19,17 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive", n);
+	}
 		else
+		{
 			if (n < 0)
 			{
 				printf("%d is negative", n);
-				else
-					printf("%d is zero", n);
 			}
-	} /* your code goes there */
+		}
+				else
+				{
+					printf("%d is zero", n);
+				} /* your code goes there */
 	return (0);
 }
