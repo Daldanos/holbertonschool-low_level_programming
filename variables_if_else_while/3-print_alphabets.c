9d380e02a4cde282327386ef16663df6cef0302a
Daldanos <adolfogsilveira@gmail.com>
2023-10-11 09:56:39 -0700

    Programming in C

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - start
+*
+* Description:
+*
+* Return: Correct output
+*/
+
+int main(void)
+{
+	int PRUEBA;
+
+	for (PRUEBA = 97; PRUEBA <= 122; PRUEBA++)
+		putchar((char)PRUEBA);
+	for (PRUEBA = 65; PRUEBA <= 90; PRUEBA++)
+		putchar((char)PRUEBA);
+	putchar('\n');
+	return (0);
+}
+
