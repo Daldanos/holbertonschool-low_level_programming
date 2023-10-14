e5335a8fe0dca88483dc099ff041b3e0bc060fe9
Daldanos <adolfogsilveira@gmail.com>
2023-10-13 19:38:27 -0700

    Proggraming in C

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - start
+*
+* Description:
+*
+* Return: Correct output
+*/
+
+int main(void)
+{
+	int PRUEBA;
+
+	for (PRUEBA = 48; PRUEBA <= 57; PRUEBA++)
+		putchar(PRUEBA);
+	putchar(',');
+	putchar(' ');
+	return (0);
+}
