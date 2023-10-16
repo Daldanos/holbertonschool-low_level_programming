53b599baa383f6a438e95d9255966abdd9dd3c34
Daldanos <adolfogsilveira@gmail.com>
2023-10-15 20:10:17 -0700

    Programming in C

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* _abs - absolute value
+* @r: int variable
+* Return: n if positive, n * -1 if negative
+*/
+int _abs(int r)
+{
+	if (r >= 0)
+		return (n);
+	else
+		return (n * (-1));
+}
