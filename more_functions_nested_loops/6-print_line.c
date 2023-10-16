4dc4f9fe3b985d018b529352ee2d7630dbc1ef30
Daldanos <adolfogsilveira@gmail.com>
2023-10-16 10:20:45 -0700

    Programming in C

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* print_line - print line
+* @n: int
+*/
+void print_line(int n)
+{
+	int t;
+
+	for (t = 0; t <= n; t++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
