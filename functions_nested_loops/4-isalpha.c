8c75f6798f80d233e8afcb759616e8adfff489c8
Daldanos <adolfogsilveira@gmail.com>
2023-10-15 19:37:03 -0700

    Programming in C

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* _isalpha - checks if is char
+* @c: char variable
+* Return: 1 if char, 0 if not
+*/
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91 || c > 96 &&  c < 123))
+		return (1);
+	else
+		return (0);
+}
