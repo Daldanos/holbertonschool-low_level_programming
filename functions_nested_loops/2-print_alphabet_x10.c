9af82f469c1c931b12a1668bd8968e2e7a634f52
Daldanos <adolfogsilveira@gmail.com>
2023-10-15 18:35:35 -0700

    Programming in C

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* print_alphabet_x10 - print alphabet
+*
+*/
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
