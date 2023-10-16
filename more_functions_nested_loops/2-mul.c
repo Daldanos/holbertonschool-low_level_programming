1c7468735ed6981ea453d0aff8ccb3f0504934ba
Daldanos <adolfogsilveira@gmail.com>
2023-10-16 07:36:54 -0700

    Programming in C

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* mul - multiplies two ints
+* @a: int
+* @b: int
+* Return: a*b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
