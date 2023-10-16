f8105342fcfb8e2a2b8afb3fbfaf4cb1c9a8f07c
Daldanos <adolfogsilveira@gmail.com>
2023-10-16 10:44:55 -0700

    Programming in C

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* print_diagonal - print diagonal
+* @n: int variable
+*/
+void print_diagonal(int n)
+{
+	int i, x;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (x = 0; x < i; x++)
+				_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+
+		}
+	else
+		_putchar('\n');
+	}
+}
