a3d3f2bcc2066b588f27249ab1dd8614bdc83542
Daldanos <adolfogsilveira@gmail.com>
2023-10-15 20:56:55 -0700

    Programming in C

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* jack_bauer - shows al the variables of a clock
+* @h @m: int variable
+*
+*/
+void jack_bauer(void)
+{
+	int h, m;
+
+	h = 0;
+
+	while (h > 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
