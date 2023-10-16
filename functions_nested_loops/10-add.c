84ca2d634a61486e0d91d8d91ba6135ffa5b0e29
Daldanos <adolfogsilveira@gmail.com>
2023-10-16 05:44:33 -0700

    Programming in C

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* add - returns the sum of 2 ints
+* @a: int variable
+* @b: int variable
+* Description: sum 2 ints
+* Return: sum of int a and b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
