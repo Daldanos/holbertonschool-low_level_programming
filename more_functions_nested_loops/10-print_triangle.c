b65976cc9ad66595946480907b493d366ef4363f
Daldanos <adolfogsilveira@gmail.com>
2023-10-16 12:18:47 -0700

    Programming in C

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* print_triangle - print triangle
+* @size: int variable
+*/
+void print_triangle(int size)
+{
+	int i, x;
+
+	if (size > 0)
+	{
+		for (i = 1; i < size; i++)
+		{
+			for (x = 1; x <= size; x++)
+			{
+				if (x <= (size - i))
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+
+		_putchar('\n');
+
+		}
+	}
+	else
+		_putchar('\n');
+}
