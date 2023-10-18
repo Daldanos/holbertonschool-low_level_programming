68dbe5666ecc9411a5c01ec8e8c5579b47f71834
Daldanos <adolfogsilveira@gmail.com>
2023-10-18 09:23:48 -0700

    Arrays

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - main function
+ * @a: pointer variable
+ * @b: pointer variable
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = temp;
+}
