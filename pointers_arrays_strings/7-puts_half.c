bdb3713a267cc9bcc08801c163a0d11aaac6dd79
Daldanos <adolfogsilveira@gmail.com>
2023-10-19 10:03:47 -0700

    Arrays

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,7 @@ void puts_half(char *str)
 
 	if (a % '2' == 0)
 	{
-		for (count = a / 2; count < a; count++)
+		for (count = a / '2'; count < a; count++)
 	{
 		rev = str[count];
 		_putchar(rev);
@@ -26,7 +26,7 @@ void puts_half(char *str)
 	else
 		if (a % '2' != 0)
 		{
-		for (count = b / 2; count < a; count++)
+		for (count = b / '2'; count < a; count++)
 
 		rev = str[count];
 		_putchar(rev);
