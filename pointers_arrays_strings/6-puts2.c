c78edbfbe6c8f04954d04a1e5583de35375d3c46
Daldanos <adolfogsilveira@gmail.com>
2023-10-19 09:26:56 -0700

    Arrays

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -11,9 +11,9 @@ void puts2(char *str)
 {
 	int count;
 	char rev;
-	int t = strlen(str) -1;
+	int t = strlen(str) - 1;
 
-	while (count < t && count != '\0')
+	while (count < t)
 	for (count = 0; t >= count; count += 2)
 	{
 		rev = str[count];
