e633036f90a35a2f45e027f5df0ea2fd45af57a7
Daldanos <adolfogsilveira@gmail.com>
2023-10-19 08:13:40 -0700

    Arrays

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - main function
+ * @str: pointer variable
+ *
+ */
+void puts2(char *str)
+{
+	int count;
+	char rev;
+
+	for (count = 0; count <= strlen(str); count += 2)
+	{
+		rev = str[count];
+		_putchar(rev);
+	}
+	_putchar('\n');
+}
