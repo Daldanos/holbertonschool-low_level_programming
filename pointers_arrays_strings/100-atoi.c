5d7eab6bbb6549b81cc8240af3324a80ffc1e9f4
Daldanos <adolfogsilveira@gmail.com>
2023-10-20 09:42:55 -0700

    Arrays

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _atoi - main function
+ * @s: pointer variable
+ *
+ */
+int _atoi(char *s)
+{
+	int a;
+	a = _atoi(s);
+	printf("%d\n", a);
+}
