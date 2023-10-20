e385110280c48242a683db022229bf9d2805db95
Daldanos <adolfogsilveira@gmail.com>
2023-10-20 08:46:38 -0700

    Arrays

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -11,12 +11,12 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
-	int j = strlen(src) -1;
 
-	while (src[i] != '\0' && i <= j)
+	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
