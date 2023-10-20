01f19b9eabeeca991542b673e0ee7ce518324f44
Daldanos <adolfogsilveira@gmail.com>
2023-10-20 05:57:13 -0700

    Arrays

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_array - main function
+ * @a: pointer variable
+ * @n: int var
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d\n", a[i]);
+	}
+	}
