89682687d45ead1630336f89597eeedd410a09e8
Daldanos <adolfogsilveira@gmail.com>
2023-10-20 07:26:31 -0700

    Arrays

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - main function
+ * @str: pointer variable
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*_strcpy(dest, src);
+	return (dest);
+}
