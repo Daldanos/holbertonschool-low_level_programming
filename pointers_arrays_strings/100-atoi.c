7869d4c86f7a7c03606cad3e7a248616f293e9d8
Daldanos <adolfogsilveira@gmail.com>
2023-10-24 06:01:40 -0700

    Arrays

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,14 +9,36 @@
  */
 int _atoi(char *s)
 {
-	int a;
-	int lar;
-	int result;
-	lar = strlen(s);
+	int sign = 1;
+	int result = 0;
 
-	for (a = 0; a < lar; a++)
+	while (*s)
 	{
-		result = result * 10 + (s[a] - '0');
+		if (*s == '-')
+		{
+			sign *= -1;
 	}
-	return (result);
+	else if (*s >= '0' && *s <= '9')
+	{
+		break;
+	}
+	s++
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		if (result > (INT_MAX - (*s - '0')) / 10)
+		{
+			if (sign == 1)
+			{
+				return INT_MAX;
+			}
+			else
+			{
+				return INT_MIN;
+			}
+		}
+		result = result * 10 + (*s - '0');
+		s++;
+	}
+	return result * sign;
 }
