bcc37710fb0f067396cc2e9087584f56c754c7a3
Daldanos <adolfogsilveira@gmail.com>
2023-10-25 06:26:14 -0700

    Arrays

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncpy - main function
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: int
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	return (dest);
+}
