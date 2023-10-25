488b694b7186553895f6bf4ce9edaaa1550faab3
Daldanos <adolfogsilveira@gmail.com>
2023-10-25 08:28:16 -0700

    Arrays

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - main function
+ * @str:string variable
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if ((str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+			continue;
+		}
+		if (str[i] == ' ' || str[i] == ',' || str[i] == ';' ||
+			str[i] == '.' || str[i] == '!' || str[i] == '?' ||
+			str[i] == '"' || str[i] == '(' || str[i] == ')' ||
+			str[i] == '{' || str[i] == '}')
+		{
+		++i;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+			continue;
+		}
+
+		}
+	}
+	return (str);
+}
