0162da9ea0396e4b0c37fd100fc8420ff96be21c
Daldanos <adolfogsilveira@gmail.com>
2023-10-25 07:59:36 -0700

    Arrays

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * string_toupper - main function
+ * @str:pointer variable
+ * Return: none
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+}
