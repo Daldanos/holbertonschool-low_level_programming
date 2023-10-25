97c7eb65a69803a896406907836b03a162072a7a
Daldanos <adolfogsilveira@gmail.com>
2023-10-25 06:42:30 -0700

    Arrays

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,18 +10,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
+		dest[i] = src[i];
 	}
-	for (j = 0; j < n && src[j] != '\0'; j++)
+	if (i < n)
 	{
-		dest[j] = src[j];
-	}
-	if (j < n)
-	{
-		dest[j] = '\0';
+		dest[i] = '\0';
 	}
 	return (dest);
 }
