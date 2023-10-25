21ebbbcd99c588a60d88aacbaa58516081d5c7e0
Daldanos <adolfogsilveira@gmail.com>
2023-10-25 07:47:05 -0700

    Arrays

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - main function
+ * @a:pointer variable
+ * @n: int variable
+ * Return: compare
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j;
+
+	while (i < n)
+	{
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+		i++;
+		n--
+	}
+	return (a);
+}
