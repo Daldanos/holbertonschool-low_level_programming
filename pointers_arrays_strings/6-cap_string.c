d28d13a50abd7d4b98f0e66ff9b6af17b9d38ed6
Daldanos <adolfogsilveira@gmail.com>
2023-10-25 10:20:34 -0700

    Arrays

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,7 @@ char *cap_string(char *str)
 			str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' ||
 			str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}'))
 		{
-			str[i] -= 32;
+			str[j] -= 32;
 		}
 
 	}
