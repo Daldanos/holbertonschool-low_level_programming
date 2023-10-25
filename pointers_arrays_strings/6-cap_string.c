6addbf28a272d84194122816a26cf0a99c11f445
Daldanos <adolfogsilveira@gmail.com>
2023-10-25 10:17:01 -0700

    Arrays

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,10 +13,10 @@ char *cap_string(char *str)
 	for (j = 0; str[j] != '\0'; j++)
 	{
 		if (str[j] >= 'a' && str[j] <= 'z')
-			if (i == 0 || str[i] == ' ' || str[i] == ',' ||
+			if (i == 0 || (str[i] == ' ' || str[i] == ',' ||
 			str[i] == ';' || str[i] == '.' || str[i] == '!' ||
 			str[i] == '?' || str[i] == '"' || str[i] == '(' ||
-			str[i] == ')' || str[i] == '{' || str[i] == '}')
+			str[i] == ')' || str[i] == '{' || str[i] == '}'))
 		{
 			str[i] -= 32;
 		}
