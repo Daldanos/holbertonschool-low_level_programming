ef00c84166432500e677ce5b7220a3a8b216e766
Daldanos <adolfogsilveira@gmail.com>
2023-10-25 06:17:39 -0700

    Arrays

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,19 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strcat - main function
+ * _strncat - main function
  * @dest: destination pointer
  * @src: source pointer
+ * @n: int
  * Return: dest
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; src[j] != '\0'; j++)
+	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
 		dest[i + j] = src[j];
 	}
