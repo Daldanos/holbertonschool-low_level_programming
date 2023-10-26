e74d76a193cc66ee16f011e326fece7054f04d00
Daldanos <adolfogsilveira@gmail.com>
2023-10-26 10:10:03 -0700

    Arrays

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * leet - main function
+ * @str:pointer variable
+ * Return: str
+ */
+char *leet(char *str)
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] = '0';
+		}
+		else if (str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] = '1';
+		}
+		else if (str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] = '3';
+		}
+		else if (str[i] == 'a' || str[i] == 'A')
+		{
+			str[i] = '4';
+		}
+		else if (str[i] == 't' || str[i] == 'T')
+		{
+			str[i] = '7';
+		}
+	}
+	return (str);
+}
