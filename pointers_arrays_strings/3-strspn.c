10f1d3b03478096688a65c30ffdba1b0b115992b
Daldanos <adolfogsilveira@gmail.com>
2023-10-29 18:59:16 -0700

    Arrays

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,20 +9,21 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0;
+	int j;
 
 	while (*s != '\0')
 	{
-		for (char *p = accept; *p != '\0'; p++)
+		for (j = 0; accept[j]; j++)
 		{
-			if (*s == *p)
+			if (*s == accept[j])
 			{
 				i++;
 				break;
 			}
-		}
-		if (*p == '\0')
-		{
-			break;
+			else if (accept[j + 1] == '\0')
+			{
+				return (i);
+			}
 		}
 		s++;
 	}
