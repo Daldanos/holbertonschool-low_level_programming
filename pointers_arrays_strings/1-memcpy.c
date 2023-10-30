cb3d212f819d2ca4fb0de5d1b3b9f659bab17349
Daldanos <adolfogsilveira@gmail.com>
2023-10-29 17:20:11 -0700

    Arrays

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - main function
+ * @dest: pointer variable
+ * @src: char
+ * @n: int
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
