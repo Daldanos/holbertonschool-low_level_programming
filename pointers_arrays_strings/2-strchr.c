b6f8442b7b9457fb5d516e09fd5918e175ff1a3f
Daldanos <adolfogsilveira@gmail.com>
2023-10-29 17:28:43 -0700

    Arrays

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - main function
+ * @s: pointer variable
+ * @c: char
+ * Return: s
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	return (NULL);
+}
