a6be4b8cbe76b56d1c8f1b9afbe0381036737445
Daldanos <adolfogsilveira@gmail.com>
2023-10-31 16:14:13 -0700

    Recursion

---
+++ recursion/3-factorial.c
@@ -0,0 +1,20 @@
+/**
+ * factorial - function
+ * @n: int
+ * Return: 1 or -1
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
