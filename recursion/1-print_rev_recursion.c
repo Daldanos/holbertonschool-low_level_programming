bd53049a71f7f77f9d0e35268230d32e411beac2
Daldanos <adolfogsilveira@gmail.com>
2023-10-31 14:28:54 -0700

    Recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _puts_rev_recursion - main function
+ * _print_rev_recursion - main function
  * @s: pointer variable
  * Return: none
  *
  */
-void _puts_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		return;
 	}
-	_puts_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
