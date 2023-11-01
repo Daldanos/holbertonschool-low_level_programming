41b8edb3389418c4e6f38b99fc76edcf4ec7efa6
Daldanos <adolfogsilveira@gmail.com>
2023-10-31 17:27:24 -0700

    Recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -11,12 +11,13 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
-	for (int i = 2; i * i <= n; i++)
+	if (n == 2)
 	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
+		return (1);
 	}
-	return (1);
+	if (n % 2 == 0)
+	{
+		return (0);
+	}
+	return (is_prime_number(n - 1);
 }
