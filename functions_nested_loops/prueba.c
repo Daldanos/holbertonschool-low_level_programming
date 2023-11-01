ea952f79ac65b65cafd05aae876cb012b6befd0f
Daldanos <adolfogsilveira@gmail.com>
2023-10-31 18:27:24 -0700

    Ending

---
+++ functions_nested_loops/prueba.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+	int n;
+
+	printf("Value of variable 'c': %d%n", c);
+        printf("Value of variable 'c': %p%n", %c);
+        printf("Value of variable 'c': %p%n", %n);
+	int n;
+}
