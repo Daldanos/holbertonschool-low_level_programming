11504a710c163a56b6016ce714155b905a4ec911
Daldanos <adolfogsilveira@gmail.com>
2023-11-05 12:04:27 -0800

    Malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - main
+ * @size: int
+ * @c: unsigned int
+ * Return: ar
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return NULL;
+
+	char *ar = malloc(size * sizeof(char));
+	
+	if (ar == NULL)
+		return NULL;
+	
+	while (size--)
+		ar[size] = c;
+	return ar;
+}
