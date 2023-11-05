b9fb604802d97c7a58c4ac28b40049a3641c1ea5
Daldanos <adolfogsilveira@gmail.com>
2023-11-05 13:25:08 -0800

    Malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -14,19 +14,19 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	
+
 	while (str[len] != '\0')
 		len++;
-	
+
 	dup = (char *)malloc((len + 1) * sizeof(char));
-	
+
 	if (dup == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < len; i++)
 		dup[i] = str[i];
 	
 	dup[len] = '\0';
-	
+
 	return (dup);
 }
