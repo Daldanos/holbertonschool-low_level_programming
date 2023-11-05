86e234c4a44a1ab460443770572aa2f9898cc6c2
Daldanos <adolfogsilveira@gmail.com>
2023-11-05 12:08:30 -0800

    Malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -7,17 +7,16 @@
  * @c: unsigned int
  * Return: ar
  */
-
 char *create_array(unsigned int size, char c)
 {
 	char *ar = malloc(size * sizeof(char));
-	
+
 	if (size == 0)
 		return NULL;
 
 	if (ar == NULL)
 		return NULL;
-	
+
 	while (size--)
 		ar[size] = c;
 	return ar;
