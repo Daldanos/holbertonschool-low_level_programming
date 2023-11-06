2bd36efc4ba8cff86371964a347dfcf59767bbf3
Daldanos <adolfogsilveira@gmail.com>
2023-11-05 16:38:06 -0800

    Malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,48 +6,39 @@
  * @s2: string
  * Return: Pointer to concatenated string
  */
-
-char *str_concat(char *s1, char *s2) 
+char *str_concat(char *s1, char *s2)
 {
-	unsigned int len1;
-	unsigned int len2;
+	unsigned int len1, len2, i, j;
 	char *conc;
-	unsigned int i;
-	unsigned int j;
 
 	len1 = 0;
 	len2 = 0;
 	i = 0;
 	j = 0;
-	
+
 	if (s1 != NULL)
 	{
 		while (s1[len1] != '\0')
 			len1++;
 	}
-	
 	if (s2 != NULL)
 	{
 		while (s2[len2] != '\0')
 			len2++;
 	}
-	
 	conc = (char *)malloc((len1 + len2 + 1) * sizeof(char));
-	
+
 	if (conc == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < len1; i++)
 	{
 		conc[i] = s1[i];
 	}
-	
 	for (j = 0; j < len2; j++)
 	{
 		conc[i + j] = s2[j];
 	}
-	
 	conc[len1 + len2] = '\0';
-	
 	return (conc);
 }
