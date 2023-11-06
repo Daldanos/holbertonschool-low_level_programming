929263c195cfd314da1c5af18573dcb061baae8b
Daldanos <adolfogsilveira@gmail.com>
2023-11-05 17:00:16 -0800

    Malloc

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees mem
+ * @grid: Pointer to the grid to be freed
+ * @height: Height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+		return;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+		grid[i] = NULL;
+	}
+	free(grid);
+	grid = NULL;
+}
