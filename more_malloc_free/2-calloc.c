3c1be48578fd1ff7f422482bc3875d606c7537bd
Daldanos <adolfogsilveira@gmail.com>
2023-11-05 17:46:34 -0800

    Malloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -23,7 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < total_size; i++)
+	for (i = 0; i < tot; i++)
 		*((char *)ptr + i) = 0;
 
 	return (ptr);
