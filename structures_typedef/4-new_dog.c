61dd13b1f8b8b106176b1d9384e7832751b955a5
Daldanos <adolfogsilveira@gmail.com>
2023-11-12 18:55:11 -0800

    Structs

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -44,3 +44,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (newDog);
 }
+
+/**
+ * free_dog - Frees memory
+ * @d: Pointer to the dog structure
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
