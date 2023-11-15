c3db6f0bbed6b547bc7a73c619990c1bb21562a9
Daldanos <adolfogsilveira@gmail.com>
2023-11-15 13:12:23 -0800

    Functions

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * print_name - Main function
+ * @name: pointer variable
+ * @f: pointer variable
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		while (*name != '\0')
+		{
+			f(*name);
+			name++;
+		}
+		_putchar('\n');
+	}
+}
