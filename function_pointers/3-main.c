1c4a9298ec8780acac6183c63550e82043f7a5a6
Daldanos <adolfogsilveira@gmail.com>
2023-11-18 22:18:02 -0800

    Structs

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int n1, n2, result;
-	int (*oper)(int, int);
+	char *op;
 
 	if (argc != 4)
 	{
@@ -20,15 +20,14 @@ int main(int argc, char *argv[])
 	}
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[3]);
+	op = argv[2];
 
-	oper = get_op_func(argv[2]);
-
-	if (oper == NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	result = oper(n1, n2);
+	result = get_op_func(op)(n1, n2);
 	printf("%d\n", result);
 
 	return (0);
