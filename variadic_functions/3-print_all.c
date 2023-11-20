28c3bea400c2a9338f83bbb5e6c19645f6570cb3
Daldanos <adolfogsilveira@gmail.com>
2023-11-19 16:14:21 -0800

    Variadic

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,44 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_all - prints all
+ * @format: types
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	int j;
+	char *separator = "";
+	print_data_t print_types[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+
+		while (print_types[j].format)
+		{
+			if (*(print_types[j].format) == format[i])
+			{
+				printf("%s", separator);
+				print_types[j].print_func(args);
+				separator = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
