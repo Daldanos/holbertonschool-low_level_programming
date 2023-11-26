75afbe90b5a6b91ba53a6a637b3dba3571f245da
Daldanos <adolfogsilveira@gmail.com>
2023-11-26 14:36:52 -0800

    Lists

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -18,13 +18,8 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%u]%s\n", h->len, h->str);
 		}
-		if (h->next)
-		{
-			printf(" ");
-		}
-
 		count++;
 		h = h->next;
 	}
