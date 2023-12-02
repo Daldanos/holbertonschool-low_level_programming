f14179b647fc98cfe5a096db82ed1340bbf95b0b
Daldanos <adolfogsilveira@gmail.com>
2023-12-02 10:50:06 -0800

    Lists

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - main function
+ * @head: pointer
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
