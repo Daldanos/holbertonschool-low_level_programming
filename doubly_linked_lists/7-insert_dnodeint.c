aa5933e7bfb6e214ad311af74cd4598510b5396a
Daldanos <adolfogsilveira@gmail.com>
2023-12-02 18:42:39 -0800

    Lists

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - main function
+ * @h: pointer
+ * @idx: unsigned int
+ * @n: int
+ * Return: return
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new;
+	dlistint_t *temp = *h;
+	unsigned int i = 0;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (temp && i < idx - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
+	if (!temp)
+		return (NULL);
+
+	if (!temp->next)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->prev = temp;
+	new->next = temp->next;
+	temp->next->prev = new;
+	temp->next = new;
+
+	return (new);
+}
