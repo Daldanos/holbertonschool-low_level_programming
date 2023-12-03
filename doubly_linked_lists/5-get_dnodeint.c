e209b3caeff6c7f711d8085a6868b503ad6e1888
Daldanos <adolfogsilveira@gmail.com>
2023-12-02 17:24:16 -0800

    Lists

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - main function
+ * @head: pointer
+ * @index: int
+ * Return: current
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current = head;
+	unsigned int cont = 0;
+
+	while (current != NULL)
+	{
+		if (cont == index)
+		{
+			return (cont);
+		}
+		cont++;
+		current = current->next;
+	}
+	return (NULL);
+}
