5cc063ac3e924961f4fc287521ed17da153b4f31
Daldanos <adolfogsilveira@gmail.com>
2023-12-03 14:23:41 -0800

    Bit

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -9,8 +9,8 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int count = 0;
-	int z;
+	unsigned long int count = 0;
+	unsigned long int z;
 
 	for (z = n ^ m; z != 0; z = z >> 1)
 	{
