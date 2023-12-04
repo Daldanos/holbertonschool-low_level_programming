18f72304c640c9de4e04f9138e46948b82a98d07
Daldanos <adolfogsilveira@gmail.com>
2023-12-03 16:25:27 -0800

    FileIO

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,36 +17,36 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		fprintf(stderr, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97); }
 	sourceFile = fopen(argv[1], "r");
-	if (!sourceFile)
-	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-		exit(98); }
 	destFile = fopen(argv[2], "w");
-	if (!destFile)
+	if (sourceFile == NULL)
 	{
-		fprintf(stderr, "Error: Can't write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
+	if (destFile == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99); }
 	while ((bytesRead = fread(buf, 1, BUF_SIZE, sourceFile)) > 0)
 	{
 		if (fwrite(buf, 1, bytesRead, destFile) != bytesRead)
 		{
-			fprintf(stderr, "Error: Can't write to file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 			exit(99); } }
 	if (ferror(sourceFile))
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
-	if (fclose(destFile) != 0)
-	{
-		fprintf(stderr, "Error: Can't close fd %d\n", fileno(sourceFile));
-		exit(100); }
-	if (fclose(destFile) != 0)
-	{
-		fprintf(stderr, "Error: Can't close fd %d\n", fileno(destFile));
-		exit(100); }
 	printf("Files copied successfully.\n");
+	if (fclose(sourceFile) == EOF)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileno(sourceFile));
+		exit(100); }
+	if (fclose(destFile) == EOF)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileno(destFile));
+		exit(100); }
 	return (0);
 }
