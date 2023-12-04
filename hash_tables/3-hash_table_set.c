acb26707ef9130f57e052da3f590820bb6de27fa
Daldanos <adolfogsilveira@gmail.com>
2023-12-03 18:52:34 -0800

    Hash

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = hash_djb2((unsigned char *)key) % ht->size;
+	unsigned long int index = key_index((const unsigned char *)key) % ht->size;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 	{
