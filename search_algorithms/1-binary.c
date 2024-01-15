70a045a0e98a8dbeafab03c75936526137011096
Daldanos <adolfogsilveira@gmail.com>
2024-01-15 08:32:51 -0800

    Making changes

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,1 +1,39 @@
 #include "search_algos.h"
+
+/**
+ * binary_search - main function
+ * @array: pointer
+ * @size: size
+ * @value: value
+ *
+ * Return: mid or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left, right, mid;
+
+	if (array == NULL)
+		return (-1);
+
+	left = 0;
+	right = size - 1;
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (mid = left; mid < right; mid++)
+			printf("%d, ", array[mid]);
+		printf("%d\n", array[mid]);
+
+		mid = left + (right - left) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
+}
